6965cee7a5aaba5ee594fe8b9783aafe12c95e1a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-09 08:47:33 +0900

    added flat component structure

---
+++ src/components/flat.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./flat.css";
+
+class Flat extends React.Component {
+  render() {
+    const title = this.props.flat.currency + this.props.flat.price +
+    " - " + this.props.flat.name;
+
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
+
+    return (
+      <div className="flat">
+        <div className="flat-picture" style={style}>
+        </div>
+        <div className="flat-title">
+        </div>
+      </div>
+    )
+  };
+}
+
+export default Flat;
--- src/components/hello.js
+++ src/components/hello.js
@@ -2,8 +2,8 @@ import React from "react";
 
 class Hello extends React.Component {
   render() {
-    return <h1>Hello from hello.js</h1>;
-  }
+    return <h1>Hello {this.props.firstName} {this.props.lastName} </h1>;
+  };
 }
 
 export default Hello;
