28fa37b59a304d3997e5369ccc82455d121e779f
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-10 12:31:49 +0900

    now fetching apis from lewagon's github and rendering them

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url).then(response => response.json())
+    .then((data) => {
+      this.setState({
+        flats: data
+      });
+    })
+  };
 
   render() {
-    const flat = {
-
-    };
     return (
       <div className="app">
         <div className="main">
           <div className="search">
             <div className="flats">
-
+              {this.state.flats.map((flat) => {
+                return < Flat flat={flat} />
+              })}
             </div>
           </div>
         </div>
