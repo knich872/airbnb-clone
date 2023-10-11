cd4d4cd158bb80b7fd03ebaf7f4632eb0e3143ae
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-11 12:07:32 +0900

    markers added to map

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './App.css';
 import Flat from './components/flat';
+import Marker from './components/flat';
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      flats: []
+      flats: [],
+      selectedFlat: null
     };
   }
 
@@ -15,16 +17,31 @@ class App extends Component {
     const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
     fetch(url).then(response => response.json())
     .then((data) => {
+      console.log(data);
       this.setState({
         flats: data
       });
     })
   };
 
+  selectFlat = (flat) => {
+    console.log(flat);
+    this.setState({
+      selectedFlat: flat
+  })
+  }
+
   render() {
-    const center = {
+    let center = {
       lat: 48.8,
       lng: 2.3
+    }
+
+    if (this.state.selectedFlat) {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng
+      }
     }
     return (
       <div className="app">
@@ -32,7 +49,9 @@ class App extends Component {
           <div className="search">
             <div className="flats">
               {this.state.flats.map((flat) => {
-                return < Flat flat={flat} />
+                return < Flat key={flat.name}
+                flat={flat}
+                selectFlat={this.selectFlat} />
               })}
             </div>
           </div>
@@ -43,6 +62,9 @@ class App extends Component {
             defaultCenter={center}
             defaultZoom={11}
           >
+            {this.state.flats.map((flat) => {
+              return < Marker key={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price} />
+            })}
           </GoogleMapReact>
         </div>
       </div>
